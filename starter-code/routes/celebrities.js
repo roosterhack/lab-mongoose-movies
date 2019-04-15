6903c748b6f50ffa95076a5242cf18147d60853d
Eddie Chung <designered@gmail.com>
2019-04-15 19:36:03 +0200

    iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,10 +2,23 @@ const express = require("express");
 const router = express.Router();
 const Celebrity = require("../models/celebrity");
 
+//list celebrities page
 router.get("/celebrities", (req, res) => {
   Celebrity.find()
     .then(data => {
       res.render("celebrities/index", { data });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+//list celebrities details page
+router.get("/celebrities/:celeId", (req, res) => {
+  const _id = req.params.celeId;
+  Celebrity.findOne({ _id })
+    .then(data => {
+      res.render("celebrities/show", { data });
     })
     .catch(err => {
       console.log(err);
